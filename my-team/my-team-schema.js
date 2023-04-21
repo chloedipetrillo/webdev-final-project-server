852c0af5fbf8cc2b7d6362dcf5edd4a6e333966d
gujralajit <gujralajit@gmail.com>
2023-04-21 14:47:25 -0400

    Updated schema to add scores.

--- my-team/my-team-schema.js
+++ my-team/my-team-schema.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 const myTeamSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: "users"},
     team : {type: Array},
+    scoresThisWeek: {type: Number, default: 0},
+    totalScores: {type: Number, default: 0}
 }, {collection: 'myTeam'});
 
 
